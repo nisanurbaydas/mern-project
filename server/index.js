ff902f16b5c0a7dbda596708f712a24549b89f0d
Nisa <n.nurb@hotmail.com>
2022-04-10 23:37:23 +0300

    Connect to the database

--- server/index.js
+++ server/index.js
@@ -8,3 +8,12 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://javascriptmastery:UG284o4CJIJSLaw3@cluster0.l8xhn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser:true, useUnifiedTopology:true})
+        .then(()=>app.listen(PORT,() => console.log(`Sever is running on port: ${PORT}`)))
+        .catch((error) => console.log(error.message));
+
+//mongoose.set('useFindAndModify', false);
