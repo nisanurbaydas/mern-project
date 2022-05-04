d4f447bc075ce52574d72305d77273bc7d30ed9e
Nisa <n.nurb@hotmail.com>
2022-05-04 18:12:24 +0300

    environmental setup

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ import bodyParser from 'body-parser';
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
+dotenv.config();
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
@@ -13,7 +15,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://javascriptmastery:UG284o4CJIJSLaw3@cluster0.l8xhn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser:true, useUnifiedTopology:true})
