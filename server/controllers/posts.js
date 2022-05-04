cd9abc48f29401deddde8792d32671b2b132c9ba
Nisa <n.nurb@hotmail.com>
2022-05-04 14:15:05 +0300

    implement liking logic to the server side

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -7,7 +7,7 @@ export const getPosts = async (req,res) => {
         const postMessages = await PostMessage.find();
         res.status(200).json(postMessages);
     } catch (error) {
-        res.status(404).json({ message: error.message});
+        res.status(404).json({ message: error.message });
     }
 }
 
@@ -18,7 +18,7 @@ export const createPost = async (req, res) => {
         await newPost.save();
         res.status(201).json(newPost);
     } catch (error) {
-        res.status(409).json({ message: error.message});
+        res.status(409).json({ message: error.message });
     }
 }
 
@@ -43,5 +43,16 @@ export const deletePost = async (req, res) => {
 
     await PostMessage.findByIdAndDelete(id);
 
-    res.json({message: 'Post deleted successfully.'});
+    res.json({ message: 'Post deleted successfully.' });
+}
+
+export const likePost = async (req, res) => {
+    const { id } = req.params;
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
+
+    const post = await PostMessage.findById(id);
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+
+    res.json(updatePost);
 }
