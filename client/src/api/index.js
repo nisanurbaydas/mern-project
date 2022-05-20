2368bff24a5909d065481bdfab7d088ae5bdc42c
Nisa <n.nurb@hotmail.com>
2022-05-20 22:55:17 +0300

    refactor

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,18 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' })
+const API = axios.create({ baseURL: 'http://localhost:5000' });
+
+API.interceptors.request.use((req) => {
+    if(localStorage.getItem('profile')) {
+        req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+    }
+
+    return req;
+});
+/*It is going to help our middleware. Our middleware can't work without that add something specific to each one of our requests.
+This going to happen on each one of our requests, this is going to happen before all of these requests.
+We have to send our token back to our backend so that the backend middleware can verify that we are actually logged in.
+*/
 
 export const fetchPosts = () => API.get('/posts');
 export const createPost = (newPost) => API.post('/posts',newPost);
